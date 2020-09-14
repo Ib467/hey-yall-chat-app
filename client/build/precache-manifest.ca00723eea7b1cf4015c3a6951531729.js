9a2ac34e31f7fecd21f7f306d8d7080dd89832b4
indi boteju <indi467@yahoo.com>
2020-09-14 10:43:19 -0700

    fixed mixed steps

---
+++ client/build/precache-manifest.ca00723eea7b1cf4015c3a6951531729.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e57b484939b28f6df83314e0fa527d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b500c240b5f167ffd080",
+    "url": "/static/css/main.5a179674.chunk.css"
+  },
+  {
+    "revision": "bfb3c3037f8fdf9b09c9",
+    "url": "/static/js/2.43a4eb56.chunk.js"
+  },
+  {
+    "revision": "30c40d404319c6bcbaa4f69d872fdf2e",
+    "url": "/static/js/2.43a4eb56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b500c240b5f167ffd080",
+    "url": "/static/js/main.e521a9d1.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "d0c57f4110dd0e34c1aaa564d8ca8faa",
+    "url": "/static/media/hey-yall.d0c57f41.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
